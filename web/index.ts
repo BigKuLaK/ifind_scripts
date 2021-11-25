44c53882d67ea7042b04be057373e721c3e177fb
qtgye <buquia.jace@gmail.com>
2021-11-25 22:57:19 +0800

    Web - Initial Web Content

--- web/index.ts
+++ web/index.ts
@@ -5,7 +5,7 @@ const PORT = 9999;
 const app = express();
 
 app.get("*", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("IFIND Scripts Works!");
 });
 
 app.listen(PORT, () => console.log(`Sever has started at port ${PORT}`));
