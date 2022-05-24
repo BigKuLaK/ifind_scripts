991ab3280fc99b11922e34862616afd8a81cff20
dev1302 <customer.success@blucursor.com>
2022-05-24 13:40:12 +0530

    Add scheduled task controller details | 24/05/2022

--- controllers/scheduledTaskController.js
+++ controllers/scheduledTaskController.js
@@ -2,12 +2,14 @@
 const ScheduledTasks = require('../scheduled-tasks');
 
 exports.scheduledTask = async(req,res) =>{
+    console.log("inside sheduled task list controller API ");
     try{
         const scheduledTask = new ScheduledTasks;
-        console.log("scheduled task : ", scheduledTask);
-        
+        const tasks = scheduledTask.tasks();
+        console.log("scheduled task : ", tasks);
+        console.log("type of : ", tasks);
     }catch(e){
-        console.log("error");
+        console.log("error :", e);
         return res.status(500).json({
             success:"false",
             msg : e.msg  
