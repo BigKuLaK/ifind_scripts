edc616f477ca56fd9355d8b715751bbba3553ca5
dev1302 <customer.success@blucursor.com>
2022-06-10 19:47:02 +0530

    task list API

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,23 +1,45 @@
 const ScheduledTasks = require('../scheduled-tasks');
+// const Queue = require('../execution-queue');
 
 exports.taskControllerApi = async (req, res) => {
     try {
         const scheduledTask = ScheduledTasks.getInstance();
+        // const execution_queue = Queue.getInstance();
         scheduledTask.init();
         const taskList = scheduledTask.list();
         const logs = scheduledTask.getLogs();
         // console.log("Logs Received ", logs);
         // taskList.map(task=>{
-        //     console.log("task name : ", task.name, " -  status : ", task.status);
-        //     console.log("task name : ", task.name, " -  isAdded : ", task.isAdded);
+            // console.log("task name : ", task.name, " -  status : ", task.status);
+            // console.log("task name : ", task.name, " -  isAdded : ", task.isAdded);
         // })
-        let addedTasks = []
+        let addedTasks = scheduledTask.getQueue();
+        if(addedTasks.length !== 0)
+        {
+            let firstQueue = addedTasks[0]
+            let status = firstQueue.status
+            let id = firstQueue.id
+            if(status == "stopped")
+            {
+                const command = "start"
+                scheduledTask.runCommand(command,id)
+            }
+            else
+            {
+                console.log("Task is Running");
+            }
+        }
+        else 
+        {
+              console.log("Queue is Empty")
+        }
+       
         // taskList.map(task=>{
         //     if(task.isAdded == "true"){
         //         addedTasks.push(task);
         //     }
         // })
-        addedTasks = scheduledTask.getQueue();
+        // console.log("AddedTasks : ", addedTasks);
         let execution_limit = ScheduledTasks.LIMIT;
         console.log("execution_limit--->", execution_limit)
         return res.status(200).json({
