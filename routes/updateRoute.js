d9d31087e89a3a33ce47aa38ced14e3e98362a0f
dev1302 <customer.success@blucursor.com>
2022-06-16 17:02:21 +0530

    Set Priority and ready task for ru from controller - 16-06-2022

--- routes/updateRoute.js
+++ routes/updateRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const limitController  = require('../controllers/limitController');
 const countdownController = require("../controllers/countdownController");
 const positionController = require("../controllers/positionController");
+const priorityController = require("../controllers/Priority");
+
 router.post('/limit', limitController.updateLimitAPI);
 router.post('/parallel', limitController.updateParalleltyAPI);
 router.post('/countdown', countdownController.updateCountdownAPI);
