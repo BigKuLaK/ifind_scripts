40dd7e678402dbfb7a148c01ed45d51f558bf78f
qtgye <buquia.jace@gmail.com>
2022-07-19 21:35:07 +0800

    Debug log for product saving after scraping

--- helpers/main-server/products.js
+++ helpers/main-server/products.js
@@ -1,6 +1,11 @@
 const { query } = require('./graphql');
 
 const addDealsProducts = async (dealType, products) => {
+  console.log(`Adding products for deal type ${dealType}.`);
+  console.log(`Endpoint host: ${process.env.MAIN_SERVER_URL}.`);
+  console.log('Sample product data:');
+  console.log(products[0]);
+
   const gql = `
       mutation AddNewDealsProducts ($deal_type:String!, $products: [ProductInput]) {
         addProductsByDeals( deal_type: $deal_type, products:$products ){
