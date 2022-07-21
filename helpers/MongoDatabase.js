2fa099a58ac6a4e65ad80baf124fa8ad764f09c2
qtgye <buquia.jace@gmail.com>
2022-07-21 10:01:27 +0800

    Increase pool size for mongodb

--- helpers/MongoDatabase.js
+++ helpers/MongoDatabase.js
@@ -4,6 +4,6 @@ if ( !process.env.MONGO_SERVER_URL ) {
     throw new Error('Missing MONGO_SERVER_URL from env.');
 }
 
-mongoose.connect(process.env.MONGO_SERVER_URL);
+mongoose.connect(process.env.MONGO_SERVER_URL, { maxPoolSize: 100 });
 
 module.exports = mongoose;
