18b51e128d0908613b261e3ad344021cfb6ed1da
qtgye <buquia.jace@gmail.com>
2022-07-22 16:59:44 +0800

    Bypass CloudFlare check for MyDealz

--- scheduled-tasks/tasks/mydealz-highlights/index.js
+++ scheduled-tasks/tasks/mydealz-highlights/index.js
@@ -145,6 +145,9 @@ const getLogs = async () => {
     const fetchedProducts = [];
     const torPage = await TOR_BROWSER.newPage();
 
+    // Bypass CloudFlare check
+    await torPage.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0')
+
     let page = 1;
     let morePageAvailable = true;
     await getRegionSources();
@@ -168,7 +171,9 @@ const getLogs = async () => {
             () => document.documentElement.outerHTML
           );
         } catch (err) {
+          await pageScreenshot(torPage, (await torPage.url()) + '--page-error-retrying');
           console.warn(err.message);
+          console.info(`Retrying...`.yellow);
           fetchTries--;
         }
       }
