2800895ae990611c1731df1cb8ca22bc3f09fa3e
Jayson Buquia <jayson.buquia@wearekettle.com>
2022-10-26 16:05:38 +0800

    Serve amazon screenshots

--- helpers/amazon/screenshotPageError.js
+++ helpers/amazon/screenshotPageError.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const fs = require('fs-extra');
+const dayjs = require('dayjs');
 const browser = require('../browser');
 
 const screenshotPageError = async (url, pageInstance) => {
+  const today = dayjs().format('YYYY-MM-DD');
   const page = pageInstance && typeof pageInstance.screenshot === 'function' ? pageInstance : await browser.getPageInstance();
   const pageHTML = await page.evaluate(
     () => document.documentElement.innerHTML
   );
   const [urlPath] = decodeURIComponent(url).split("?");
   const directoryTree = urlPath.replace(/^.+amazon[^/]+\//i, "").split("/");
-  const dirPath = path.resolve(__dirname, "page-errors", ...directoryTree);
+  const dirPath = path.resolve(__dirname, "../../public/screenshots", today, "amazon-page-errors", ...directoryTree);
   fs.ensureDirSync(dirPath);
   await page.screenshot({ path: path.resolve(dirPath, "index.jpg") });
   fs.outputFileSync(path.resolve(dirPath, "index.html"), pageHTML);
